1e4a258e7a41d043e5ba75149e984656221268b8
Karlo Verde <karlo.verde.1996@gmail.com>
2020-08-10 04:13:44 -0500

    Discover BT devices

--- lib/data/data_source/hardware/bluetooth/bluetooth.dart
+++ lib/data/data_source/hardware/bluetooth/bluetooth.dart
@@ -1,11 +1,34 @@
 import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 import 'package:meta/meta.dart';
+
+import '../../../../core/loggers/data/hardware_data_source.dart';
+import '../../../../domain/entities/bluetooth_state/bluetooth_state_entity.dart';
+import '../../../../domain/entities/bt_device/bt_device_entity.dart';
+import '../../../models/bluetooth_state/bluetooth_state_model.dart';
+import '../../../models/bt_device/bt_device_model.dart';
+
+part 'bluetooth.freezed.dart';
+
+@freezed
+abstract class StateStreamException with _$StateStreamException {
+  const factory StateStreamException.unexpected() =
+      _StateStreamExceptionUnexpected;
+}
+
+@freezed
+abstract class DiscoveredDeviceStreamException
+    with _$DiscoveredDeviceStreamException {
+  const factory DiscoveredDeviceStreamException.unexpected() =
+      _DiscoveredDeviceStreamExceptionUnexpected;
+}
 
 abstract class BluetoothHardwareDataSourceDec {
   const BluetoothHardwareDataSourceDec();
 
-  Stream<BluetoothState> stateStream();
+  Stream<BluetoothStateEntity> stateStream();
+  Stream<BtDeviceEntity> discoveredDeviceStream();
 }
 
 @LazySingleton(as: BluetoothHardwareDataSourceDec)
@@ -17,8 +40,51 @@ class BluetoothHardwareDataSourceImp extends BluetoothHardwareDataSourceDec {
   });
 
   @override
-  Stream<BluetoothState> stateStream() async* {
-    yield await bluetoothSerial.state;
-    yield* bluetoothSerial.onStateChanged();
+  Stream<BluetoothStateEntity> stateStream() async* {
+    try {
+      yield BluetoothStateModel.fromBluetoothState(
+        await bluetoothSerial.state,
+      );
+    } catch (e) {
+      kHardwareDataSourceLogger.e(e.runtimeType);
+      throw const StateStreamException.unexpected();
+    }
+
+    yield* bluetoothSerial
+        .onStateChanged()
+        .map(
+          (bluetoothState) => BluetoothStateModel.fromBluetoothState(
+            bluetoothState,
+          ),
+        )
+        .handleError(
+      // HACK: Error mapping.
+      (e) {
+        kHardwareDataSourceLogger.e(e.runtimeType);
+        throw const StateStreamException.unexpected();
+      },
+    );
+  }
+
+  @override
+  Stream<BtDeviceEntity> discoveredDeviceStream() {
+    return bluetoothSerial
+        .startDiscovery()
+        .map(
+          // (bluetoothDiscoveryResult) => const BtDeviceEntity(
+          //   macAddress: 'mac',
+          //   name: 'name',
+          // ),
+          (bluetoothDiscoveryResult) => BtDeviceModel.fromBluetoothDevice(
+            bluetoothDiscoveryResult.device,
+          ),
+        )
+        .handleError(
+      // HACK: Error mapping.
+      (e) {
+        kHardwareDataSourceLogger.e(e.runtimeType);
+        throw const DiscoveredDeviceStreamException.unexpected();
+      },
+    );
   }
 }
---
+++ lib/data/data_source/hardware/bluetooth/bluetooth.freezed.dart
@@ -0,0 +1,287 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
+
+part of 'bluetooth.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+class _$StateStreamExceptionTearOff {
+  const _$StateStreamExceptionTearOff();
+
+// ignore: unused_element
+  _StateStreamExceptionUnexpected unexpected() {
+    return const _StateStreamExceptionUnexpected();
+  }
+}
+
+// ignore: unused_element
+const $StateStreamException = _$StateStreamExceptionTearOff();
+
+mixin _$StateStreamException {
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result unexpected(),
+  });
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result unexpected(),
+    @required Result orElse(),
+  });
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result unexpected(_StateStreamExceptionUnexpected value),
+  });
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result unexpected(_StateStreamExceptionUnexpected value),
+    @required Result orElse(),
+  });
+}
+
+abstract class $StateStreamExceptionCopyWith<$Res> {
+  factory $StateStreamExceptionCopyWith(StateStreamException value,
+          $Res Function(StateStreamException) then) =
+      _$StateStreamExceptionCopyWithImpl<$Res>;
+}
+
+class _$StateStreamExceptionCopyWithImpl<$Res>
+    implements $StateStreamExceptionCopyWith<$Res> {
+  _$StateStreamExceptionCopyWithImpl(this._value, this._then);
+
+  final StateStreamException _value;
+  // ignore: unused_field
+  final $Res Function(StateStreamException) _then;
+}
+
+abstract class _$StateStreamExceptionUnexpectedCopyWith<$Res> {
+  factory _$StateStreamExceptionUnexpectedCopyWith(
+          _StateStreamExceptionUnexpected value,
+          $Res Function(_StateStreamExceptionUnexpected) then) =
+      __$StateStreamExceptionUnexpectedCopyWithImpl<$Res>;
+}
+
+class __$StateStreamExceptionUnexpectedCopyWithImpl<$Res>
+    extends _$StateStreamExceptionCopyWithImpl<$Res>
+    implements _$StateStreamExceptionUnexpectedCopyWith<$Res> {
+  __$StateStreamExceptionUnexpectedCopyWithImpl(
+      _StateStreamExceptionUnexpected _value,
+      $Res Function(_StateStreamExceptionUnexpected) _then)
+      : super(_value, (v) => _then(v as _StateStreamExceptionUnexpected));
+
+  @override
+  _StateStreamExceptionUnexpected get _value =>
+      super._value as _StateStreamExceptionUnexpected;
+}
+
+class _$_StateStreamExceptionUnexpected
+    implements _StateStreamExceptionUnexpected {
+  const _$_StateStreamExceptionUnexpected();
+
+  @override
+  String toString() {
+    return 'StateStreamException.unexpected()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) || (other is _StateStreamExceptionUnexpected);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result unexpected(),
+  }) {
+    assert(unexpected != null);
+    return unexpected();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result unexpected(),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (unexpected != null) {
+      return unexpected();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required Result unexpected(_StateStreamExceptionUnexpected value),
+  }) {
+    assert(unexpected != null);
+    return unexpected(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result unexpected(_StateStreamExceptionUnexpected value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (unexpected != null) {
+      return unexpected(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _StateStreamExceptionUnexpected implements StateStreamException {
+  const factory _StateStreamExceptionUnexpected() =
+      _$_StateStreamExceptionUnexpected;
+}
+
+class _$DiscoveredDeviceStreamExceptionTearOff {
+  const _$DiscoveredDeviceStreamExceptionTearOff();
+
+// ignore: unused_element
+  _DiscoveredDeviceStreamExceptionUnexpected unexpected() {
+    return const _DiscoveredDeviceStreamExceptionUnexpected();
+  }
+}
+
+// ignore: unused_element
+const $DiscoveredDeviceStreamException =
+    _$DiscoveredDeviceStreamExceptionTearOff();
+
+mixin _$DiscoveredDeviceStreamException {
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result unexpected(),
+  });
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result unexpected(),
+    @required Result orElse(),
+  });
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required
+        Result unexpected(_DiscoveredDeviceStreamExceptionUnexpected value),
+  });
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result unexpected(_DiscoveredDeviceStreamExceptionUnexpected value),
+    @required Result orElse(),
+  });
+}
+
+abstract class $DiscoveredDeviceStreamExceptionCopyWith<$Res> {
+  factory $DiscoveredDeviceStreamExceptionCopyWith(
+          DiscoveredDeviceStreamException value,
+          $Res Function(DiscoveredDeviceStreamException) then) =
+      _$DiscoveredDeviceStreamExceptionCopyWithImpl<$Res>;
+}
+
+class _$DiscoveredDeviceStreamExceptionCopyWithImpl<$Res>
+    implements $DiscoveredDeviceStreamExceptionCopyWith<$Res> {
+  _$DiscoveredDeviceStreamExceptionCopyWithImpl(this._value, this._then);
+
+  final DiscoveredDeviceStreamException _value;
+  // ignore: unused_field
+  final $Res Function(DiscoveredDeviceStreamException) _then;
+}
+
+abstract class _$DiscoveredDeviceStreamExceptionUnexpectedCopyWith<$Res> {
+  factory _$DiscoveredDeviceStreamExceptionUnexpectedCopyWith(
+          _DiscoveredDeviceStreamExceptionUnexpected value,
+          $Res Function(_DiscoveredDeviceStreamExceptionUnexpected) then) =
+      __$DiscoveredDeviceStreamExceptionUnexpectedCopyWithImpl<$Res>;
+}
+
+class __$DiscoveredDeviceStreamExceptionUnexpectedCopyWithImpl<$Res>
+    extends _$DiscoveredDeviceStreamExceptionCopyWithImpl<$Res>
+    implements _$DiscoveredDeviceStreamExceptionUnexpectedCopyWith<$Res> {
+  __$DiscoveredDeviceStreamExceptionUnexpectedCopyWithImpl(
+      _DiscoveredDeviceStreamExceptionUnexpected _value,
+      $Res Function(_DiscoveredDeviceStreamExceptionUnexpected) _then)
+      : super(_value,
+            (v) => _then(v as _DiscoveredDeviceStreamExceptionUnexpected));
+
+  @override
+  _DiscoveredDeviceStreamExceptionUnexpected get _value =>
+      super._value as _DiscoveredDeviceStreamExceptionUnexpected;
+}
+
+class _$_DiscoveredDeviceStreamExceptionUnexpected
+    implements _DiscoveredDeviceStreamExceptionUnexpected {
+  const _$_DiscoveredDeviceStreamExceptionUnexpected();
+
+  @override
+  String toString() {
+    return 'DiscoveredDeviceStreamException.unexpected()';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _DiscoveredDeviceStreamExceptionUnexpected);
+  }
+
+  @override
+  int get hashCode => runtimeType.hashCode;
+
+  @override
+  @optionalTypeArgs
+  Result when<Result extends Object>({
+    @required Result unexpected(),
+  }) {
+    assert(unexpected != null);
+    return unexpected();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeWhen<Result extends Object>({
+    Result unexpected(),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (unexpected != null) {
+      return unexpected();
+    }
+    return orElse();
+  }
+
+  @override
+  @optionalTypeArgs
+  Result map<Result extends Object>({
+    @required
+        Result unexpected(_DiscoveredDeviceStreamExceptionUnexpected value),
+  }) {
+    assert(unexpected != null);
+    return unexpected(this);
+  }
+
+  @override
+  @optionalTypeArgs
+  Result maybeMap<Result extends Object>({
+    Result unexpected(_DiscoveredDeviceStreamExceptionUnexpected value),
+    @required Result orElse(),
+  }) {
+    assert(orElse != null);
+    if (unexpected != null) {
+      return unexpected(this);
+    }
+    return orElse();
+  }
+}
+
+abstract class _DiscoveredDeviceStreamExceptionUnexpected
+    implements DiscoveredDeviceStreamException {
+  const factory _DiscoveredDeviceStreamExceptionUnexpected() =
+      _$_DiscoveredDeviceStreamExceptionUnexpected;
+}

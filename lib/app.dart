c025204c383d1ed1d842670ba09465180b1e6bd5
Karlo Verde <karlo.verde.1996@gmail.com>
2020-08-09 21:24:09 -0500

    Cubit provided

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,27 @@
-import 'package:classical_bluetooth_app/presentation/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'core/injection/injection.dart';
+import 'presentation/screens/bluetooth_status/bluetooth_status.dart';
+import 'presentation/ui_logic_holders/bluetooth_state_cubit/bluetooth_state_cubit.dart';
 
 class MyApp extends StatelessWidget {
   const MyApp();
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Material App',
-      home: HomeScreen(),
+      home: MultiBlocProvider(
+        providers: [
+          BlocProvider<BluetoothStateCubit>(
+            create: (_) =>
+                getIt<BluetoothStateCubit>()..subscribeToBluetoothState(),
+          ),
+        ],
+        child: const BluetoothStatusScreen(),
+      ),
     );
   }
 }

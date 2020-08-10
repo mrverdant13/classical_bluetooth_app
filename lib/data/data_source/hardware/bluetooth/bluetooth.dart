87c1f28b6f633c17b86ef6954b19775c8240e7b0
Karlo Verde <karlo.verde.1996@gmail.com>
2020-08-09 21:09:22 -0500

    BT data source create

---
+++ lib/data/data_source/hardware/bluetooth/bluetooth.dart
@@ -0,0 +1,21 @@
+import 'package:flutter_bluetooth_serial/flutter_bluetooth_serial.dart';
+import 'package:injectable/injectable.dart';
+import 'package:meta/meta.dart';
+
+abstract class BluetoothHardwareDataSourceDec {
+  const BluetoothHardwareDataSourceDec();
+
+  Stream<BluetoothState> stateStream();
+}
+
+@LazySingleton(as: BluetoothHardwareDataSourceDec)
+class BluetoothHardwareDataSourceImp extends BluetoothHardwareDataSourceDec {
+  final FlutterBluetoothSerial bluetoothSerial;
+
+  const BluetoothHardwareDataSourceImp({
+    @required this.bluetoothSerial,
+  });
+
+  @override
+  Stream<BluetoothState> stateStream() => bluetoothSerial.onStateChanged();
+}

5d022a1ae72560092e714c96c1a8860173017e20
Karlo Verde <karlo.verde.1996@gmail.com>
2020-08-11 02:21:07 -0500

    No action functions create

---
+++ lib/core/other_helpers/no_action_functions.dart
@@ -0,0 +1,8 @@
+// ignore: non_constant_identifier_names
+void NoActionWithNoArguments() {}
+
+// ignore: non_constant_identifier_names
+void NoActionWithOneArgument(_, __) {}
+
+// ignore: non_constant_identifier_names
+void NoActionWithTwoArguments(_, __, ___) {}
